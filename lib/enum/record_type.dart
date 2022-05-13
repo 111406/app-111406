b9c5ceb4993c3be5fca358a8556f75d03ffdb31d
azhe922 <zsda5858sda@gamil.com>
2022-05-13 22:09:39 +0800

    adjust enums, adding get value and parse function

--- lib/enum/record_type.dart
+++ lib/enum/record_type.dart
@@ -1,1 +1,28 @@
-enum RecordType { training, examination }
+class RecordType {
+  final String string;
+  final int code;
+  const RecordType._(this.string, this.code);
+
+  static const training = RecordType._('training', 0);
+  static const examination = RecordType._('examination', 1);
+
+  static const values = [training, examination];
+
+  static RecordType? parse(int value) {
+    switch (value) {
+      case 0:
+        return RecordType.training;
+      case 1:
+        return RecordType.examination;
+      default:
+        return null;
+    }
+  }
+
+  int get value => code;
+
+  @override
+  String toString() {
+    return 'RecordType.$string';
+  }
+}
