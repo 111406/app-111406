87b5d64de54e6346265d41c3fc109b715a74a287
Fang <10846009@ntub.edu.tw>
2022-09-05 00:45:30 +0800

    忘記密碼後端串接(等後端修改)

--- lib/screen/forgot_password/forgotpassword02.dart
+++ lib/screen/forgot_password/forgotpassword02.dart
@@ -1,150 +1,137 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:sport_app/screen/components/app_logo.dart';
+import 'package:sport_app/screen/components/button.dart';
+import 'package:sport_app/screen/components/page_title.dart';
+import 'package:sport_app/screen/components/textfield_inputbox.dart';
+import 'package:sport_app/screen/login/login.dart';
 import 'package:sport_app/theme/color.dart';
+import 'package:sport_app/utils/alertdialog.dart';
+import 'package:sport_app/utils/http_request.dart';
 
 class ForgotPassword02 extends StatefulWidget {
   const ForgotPassword02({Key? key}) : super(key: key);
+  static const String routeName = "/forgotpassword/next";
 
   @override
   State<ForgotPassword02> createState() => _ForgotPassword02State();
 }
 
-Widget _forgotPsNewPsTF() {
-  //新密碼框
-  return Column(
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: [
-      const Text(
-        '新密碼',
-        style: TextStyle(
-          color: primaryColor,
-          fontSize: 24,
-          fontWeight: FontWeight.bold,
-        ),
-      ),
-      const SizedBox(height: 10.0),
-      Container(
-        alignment: Alignment.centerLeft,
-        height: 50,
-        child: const TextField(
-          style: TextStyle(color: Colors.black),
-          decoration: InputDecoration(
-            contentPadding: EdgeInsets.only(top: 10),
-            prefixIcon: Icon(Icons.lock, color: primaryColor),
-            hintText: '新密碼',
-            hintStyle: TextStyle(color: primaryColor),
-            enabledBorder: OutlineInputBorder(
-              borderSide: BorderSide(color: primaryColor, width: 1),
-            ),
-            focusedBorder: OutlineInputBorder(
-              borderSide: BorderSide(color: primaryColor, width: 1),
-            ),
-          ),
-        ),
-      ),
-    ],
-  );
-}
-
-Widget _forgotPsNewPsConf() {
-  //確認新密碼
-  return Column(
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: [
-      const Text(
-        '確認密碼',
-        style: TextStyle(
-          color: primaryColor,
-          fontSize: 24,
-          fontWeight: FontWeight.bold,
-        ),
-      ),
-      const SizedBox(height: 10.0),
-      Container(
-        alignment: Alignment.centerLeft,
-        height: 50,
-        child: const TextField(
-          //keyboardType: TextInputType.emailAddress,
-          style: TextStyle(color: Colors.black),
-          decoration: InputDecoration(
-            contentPadding: EdgeInsets.only(top: 10),
-            prefixIcon: Icon(Icons.lock, color: primaryColor),
-            hintText: '確認密碼',
-            hintStyle: TextStyle(color: primaryColor),
-            enabledBorder: OutlineInputBorder(
-              borderSide: BorderSide(color: primaryColor, width: 1),
-            ),
-            focusedBorder: OutlineInputBorder(
-              borderSide: BorderSide(color: primaryColor, width: 1),
-            ),
-          ),
-        ),
-      ),
-    ],
-  );
-}
-
-Widget _forgotPsLoginBtn() {
-  //重新登入按紐
-  return Container(
-    padding: const EdgeInsets.symmetric(vertical: 20),
-    height: 90,
-    width: double.infinity,
-    child: ElevatedButton(
-      onPressed: () {},
-      child: const Text('重新登入', style: TextStyle(fontSize: 24)),
-      style: ElevatedButton.styleFrom(primary: primaryColor),
-    ),
-  );
-}
-
 class _ForgotPassword02State extends State<ForgotPassword02> {
+  final newPasswordController = TextEditingController();
+  final confirmNewPasswordController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    newPasswordController.dispose();
+    confirmNewPasswordController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          SizedBox(
-            height: double.infinity,
-            child: SingleChildScrollView(
-              physics: const AlwaysScrollableScrollPhysics(),
-              padding: const EdgeInsets.symmetric(
-                horizontal: 20.0,
-                vertical: 80.0,
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+      body: SafeArea(
+        child: Container(
+          margin: const EdgeInsets.symmetric(horizontal: 35, vertical: 40),
+          child: Column(
+            children: [
+              appLogo(),
+              pageTitle('重新建立密碼'),
+              const SizedBox(height: 30),
+              Column(
                 children: [
-                  Container(
-                    alignment: Alignment.center,
-                    child: Image.asset(
-                      'assets/icon/logo01.png',
-                      fit: BoxFit.contain,
-                      width: 150,
-                    ),
+                  textField(
+                    textFieldName: '新密碼',
+                    hintText: '請輸入新密碼',
+                    obscureText: true,
+                    icon: Icons.lock,
+                    controller: newPasswordController,
                   ),
-                  Container(
-                    alignment: Alignment.center,
-                    child: const Text(
-                      '忘記密碼',
-                      style: TextStyle(
-                        decoration: TextDecoration.none,
-                        color: primaryColor,
-                        fontSize: 30.0,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
+                  const SizedBox(height: 10),
+                  textField(
+                    textFieldName: '確認密碼',
+                    hintText: '確認密碼',
+                    obscureText: true,
+                    icon: Icons.lock,
+                    controller: confirmNewPasswordController,
                   ),
-                  const SizedBox(height: 20),
-                  _forgotPsNewPsTF(),
-                  const SizedBox(height: 20),
-                  _forgotPsNewPsConf(),
-                  const SizedBox(height: 30),
-                  _forgotPsLoginBtn(),
                 ],
               ),
-            ),
+              const SizedBox(height: 40),
+              mainBtn(
+                text: '重新登入',
+                onPressed: () async {
+                  final newPassword = newPasswordController.text,
+                      confirmNewPassword = confirmNewPasswordController.text;
+
+                  bool _textFieldIsNotEmpty =
+                      (newPassword.isNotEmpty || confirmNewPassword.isNotEmpty);
+                  bool _passwordCheck = (newPassword == confirmNewPassword);
+
+                  if (!_textFieldIsNotEmpty) {
+                    showAlertDialog(
+                      context,
+                      title: '',
+                      message: '',
+                    );
+                  }
+                  if (!_passwordCheck) {
+                    showAlertDialog(
+                      context,
+                      title: '',
+                      message: '',
+                    );
+                  }
+
+                  SharedPreferences prefs =
+                      await SharedPreferences.getInstance();
+                  final email = prefs.getString("email");
+
+                  String requestData = """{
+                    "email": "$email", 
+                    "password": "$newPassword"
+                  }""";
+
+                  try {
+                    await HttpRequest()
+                        .post('${HttpURL.host}/api/user/update/password',
+                            requestData)
+                        .then(
+                          (response) async {},
+                        );
+                    showAlertDialog(
+                      context,
+                      title: '修改成功',
+                      message: '',
+                    );
+                    Navigator.pushReplacementNamed(
+                        context, LoginPage.routeName);
+                  } on Exception catch (e) {
+                    // TODO
+                    showAlertDialog(
+                      context,
+                      title: '',
+                      message: '',
+                    );
+                  }
+                },
+              ),
+              const SizedBox(height: 20),
+              underScoreBtn(
+                text: '取消 / 返回',
+                onPressed: () {
+                  Navigator.pushReplacementNamed(context, LoginPage.routeName);
+                },
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
