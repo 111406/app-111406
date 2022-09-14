6b3e072b6cbf8ec3dc2179d741815c4f759567dc
Fang <10846009@ntub.edu.tw>
2022-09-15 00:13:49 +0800

    add editTextColor

--- lib/theme/color.dart
+++ lib/theme/color.dart
@@ -5,4 +5,5 @@ const secondColor = Color(0xff4E605E);
 const thirdColor = Color(0xffEAEAEA);
 
 const textColor = Color(0xff7E7E7E);
+const editTextColor = Colors.blue;
 const backgroundColor = Color(0xffEAEAEA);
